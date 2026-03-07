dbdc34805e3507d55c607eda756a8ca0f02bbfa7
Adithya P <prasadadithya714@gmail.com>
2026-03-07 21:14:53 +0530

    Add AWS SDK dependencies and configure S3 and STS clients

---
+++ src/main/kotlin/com/HLS_user_service/config/AWSConfig.kt
@@ -0,0 +1,25 @@
+package com.HLS_user_service.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.s3.presigner.S3Presigner
+import software.amazon.awssdk.services.sts.StsClient
+
+
+@Configuration
+class AwsConfig {
+
+    @Bean
+    fun s3Client(): S3Client =
+        S3Client.builder().build()
+
+    @Bean
+    fun s3Presigner(): S3Presigner =
+        S3Presigner.builder().build()
+
+    @Bean
+    fun stsClient(): StsClient =
+        StsClient.builder()
+            .build()
+}
